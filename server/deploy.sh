22c635c2faf4b08caa9a5330882392a78e442562
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2021-04-02 16:29:04 +0100

    Update deploy.sh

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,4 +5,4 @@ read VERSION
 
 docker build -t lhowsam/ts-node-starter:$VERSION .
 docker push lhowsam/ts-node-starter:$VERSION
-ssh root@64.227.13.208 "docker pull lhowsam/ts-node-starter:$VERSION && docker tag lhowsam/ts-node-starter:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+ssh root@ip-addr "docker pull lhowsam/ts-node-starter:$VERSION && docker tag lhowsam/ts-node-starter:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
